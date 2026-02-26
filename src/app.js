5ef0ac84f8ad804c2b7e59b4459d6b963d1dc38b
Damindu <dss@tiqri.com>
2026-02-26 18:20:25 +0530

    fix (API error handler): Changed 404 error message

--- src/app.js
+++ src/app.js
@@ -1,26 +1,28 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const express = require("express");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // 404 handler
 app.use((req, res) => {
-  res.status(404).json({ error: 'Not Found' });
+  res.status(404).json({ error: "End point Not Found" });
 });
 
 // Error handler
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
+  res
+    .status(err.status || 500)
+    .json({ error: err.message || "Internal Server Error" });
 });
 
 module.exports = app;
